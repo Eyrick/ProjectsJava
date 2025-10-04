1d1924c33ecd6f69a52ca5428389b945f755a0f3
Eyrick <eyrick.glv@gmail.com>
2025-10-04 20:23:53 -0300

    Adiçao de Metodos e Classes Separadas

--- src/Pacotes/Eletronico.java
+++ src/Pacotes/Eletronico.java
@@ -8,6 +8,7 @@ public class Eletronico extends Produtos_Fisicos{
 
     public Eletronico(int id_produto, String nome, String descricao, double preco_base, int quant_estoque, String marca, String modelo, int voltagem, int garantia_meses) {
         super(id_produto, nome, descricao, preco_base, quant_estoque);
+        this.marca = marca;
         this.modelo = modelo;
         this.voltagem = voltagem;
         this.garantia_meses = garantia_meses;
@@ -16,6 +17,7 @@ public class Eletronico extends Produtos_Fisicos{
     @Override
     public void exibirDetalhes(){
         super.exibirDetalhes();
+        System.out.println("Marca: " + marca);
         System.out.println("Modelo: " + modelo);
         System.out.printf("Voltagem: %dV\n",voltagem);
         System.out.printf("Garantia: %d meses\n", garantia_meses);
